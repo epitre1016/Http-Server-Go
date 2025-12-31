b25768b0f0a09cdef445f44e7c322cbf9df5158d
epitre1016 <epitre7@gmail.com>
2025-12-30 20:50:14 -0500

    Add URL parsing to find name in URLPath

--- server.go
+++ server.go
@@ -3,8 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(response, "20")
+
+	player := strings.TrimPrefix(request.URL.Path, "/players/")
+
+	fmt.Fprint(response, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+
+	if name == "Pepe" {
+		return "20"
+	}
+
+	if name == "Charlie" {
+		return "10"
+	}
+
+	return ""
 }

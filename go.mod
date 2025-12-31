67f2aa51bc4f71282d3812320990e000f10a31ba
epitre1016 <epitre7@gmail.com>
2025-12-30 20:22:27 -0500

    Bare Bones Start add simple get request unit test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/epitre1016/http-server
+
+go 1.25.5
---
+++ server_test.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPlayer(t *testing.T) {
+	t.Run("request player score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/TestPlayer", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
+}
